f8572f1edc187173ea8a6155d59692cdd7b54044
Turzhyk <andrewurbro@yahoo.com>
2026-07-16 07:44:20 +0200

    feat: ex3: new section

--- src/Examples/Dentysta/About.tsx
+++ src/Examples/Dentysta/About.tsx
@@ -1,8 +1,34 @@
-import { LaptopMinimalCheck, Leaf, Star } from "lucide-react";
+import {
+  CheckCheckIcon,
+  LaptopMinimalCheck,
+  Leaf,
+  LucideHeater,
+  Star,
+} from "lucide-react";
 import styles from "./styles.module.css";
 import { CalendarSvg } from "./SVG/CalendarSvg";
 import { CheckmarkThinSvg } from "./SVG/CheckmarkThinSvg";
 import { useInView } from "./useInView";
+
+import React from "react";
+
+export const AboutItem = ({
+  title,
+  icon,
+}: {
+  title: string;
+  icon: React.ReactNode;
+}) => {
+  return (
+    <div className={styles.item}>
+     <p>{title}</p> 
+      <div className={styles.icon}>
+        <CheckCheckIcon size={70} color="currentColor" />
+      </div>
+    </div>
+  );
+};
+
 export default function About() {
   const { ref, isVisible } = useInView(0.5);
   return (
@@ -10,8 +36,15 @@ export default function About() {
       ref={ref}
       className={styles.about + " " + (isVisible ? styles.visible : "")}
       id="about"
-    > <h2> Dlaczego pacjenci wybierają nas</h2>
-      <div className={styles.content}></div>
+    >
+      <h2>Komfort i bezpieczeństwo podczas każdej wizyty</h2>
+      <p>Nowoczesna stomatologia, komfort leczenia i indywidualne podejście do każdego pacjenta.</p>
+      <div className={styles.content}>
+        <AboutItem title="Leczenie bez bólu" icon={<LucideHeater />} />
+        <AboutItem title="Krótkie terminy" icon={<LucideHeater />} />
+        <AboutItem title="Zdjęcia RTG na miejscu" icon={<LucideHeater />} />
+        <AboutItem title="Nowoczesny sprzęt" icon={<LucideHeater />} />
+      </div>
     </section>
   );
 }
